5b763a9793383c21315a5ef0dcde713bd4879b3a
Alexandra Sommar <alexandra@sommar.se>
2017-03-06 14:46:54 +0100

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles/css/style.css">
+    <title></title>
+  </head>
+  <body>
+    <h1>Hej hej</h1>
+
+  </body>
+</html>
